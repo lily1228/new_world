4a7c8a39c026ecf2ced62ea94115740108b84a2c
Lily <lilia_azucena@mail.ru>
2012-02-14 23:20:01 -0800

    its a new world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewWorld::Application.config.secret_token = 'f9a8195f59608e43b36bcfa9d731147b107deb0aaca1001b6baf0a1db28be5d621f7152fd2db521cdb5a905784a9d4f6f69038fc247fad292dd0f98fbebdf5a7'
